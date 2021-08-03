0005dfc2a87c5262f958eb8797c39aa9e1ec3117
Austin Crim <crim.austin@principal.com>
2021-08-03 11:46:25 -0500

    adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,11 @@
+import adapter from '@sveltejs/adapter-netlify'
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
-	}
-};
+  kit: {
+    target: '#svelte',
+    adapter: adapter()
+  }
+}
 
-export default config;
+export default config
