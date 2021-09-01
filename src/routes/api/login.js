609f58a3fe1e5beb93afbccb4b488a7339c278dd
Austin Crim <crim.austin@principal.com>
2021-09-01 10:02:12 -0500

    remove argon

--- src/routes/api/login.js
+++ src/routes/api/login.js
@@ -1,57 +1,59 @@
 import prisma from '$lib/prisma'
-import argon from 'argon2'
+// import argon from 'argon2'
 
 /**
  * @type {import('@sveltejs/kit').RequestHandler}
  */
 export const post = async ({ body }) => {
-  const email = body.get('email')
-  const password = body.get('password')
+	const email = body.get('email')
+	const password = body.get('password')
 
-  if (!email || !password) {
-    return {
-      status: 400
-    }
-  }
+	if (!email || !password) {
+		return {
+			status: 400
+		}
+	}
 
-  const hashed = await argon.hash(password)
+	// const hashed = await argon.hash(password)
+	const hashed = password + '123'
 
-  let session, user
+	let session, user
 
-  user = await prisma.user.findUnique({
-    where: {
-      email
-    }
-  })
+	user = await prisma.user.findUnique({
+		where: {
+			email
+		}
+	})
 
-  if (user?.email) {
-    const match = await argon.verify(user.password, password)
-    if (!match) {
-      return {
-        status: 400
-      }
-    }
-  } else {
-    user = await prisma.user.create({
-      data: {
-        email,
-        password: hashed
-      }
-    })
-  }
+	if (user?.email) {
+		// const match = await argon.verify(user.password, password)
+		const match = user.password === password
+		if (!match) {
+			return {
+				status: 400
+			}
+		}
+	} else {
+		user = await prisma.user.create({
+			data: {
+				email,
+				password: hashed
+			}
+		})
+	}
 
-  session = await prisma.session.create({
-    data: {
-      user: { connect: { email: user.email } },
-      expiresAt: Date.now() + (1000 * 60 * 20)
-    }
-  })
+	session = await prisma.session.create({
+		data: {
+			user: { connect: { email: user.email } },
+			expiresAt: Date.now() + 1000 * 60 * 20
+		}
+	})
 
-  return {
-    status: 302,
-    headers: {
-      'set-cookie': `session_id=${session.id}; Path=/; HttpOnly; Secure;`,
-      location: '/'
-    }
-  }
+	return {
+		status: 302,
+		headers: {
+			'set-cookie': `session_id=${session.id}; Path=/; HttpOnly; Secure;`,
+			location: '/'
+		}
+	}
 }
