5510b6e1b221fb4d80097975dd013c6147456c37
Austin Crim <crim.austin@principal.com>
2021-09-01 20:00:23 -0500

    switch to vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,10 @@
-import netlify from '@sveltejs/adapter-netlify'
+import vercel from '@sveltejs/adapter-vercel'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		target: '#svelte',
-		adapter: netlify({
+		adapter: vercel({
 			esbuild: (opts) => ({ ...opts, external: ['mock-aws-s3', 'aws-sdk', 'nock'] })
 		}),
 		vite: {
