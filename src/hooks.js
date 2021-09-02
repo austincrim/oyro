a887e1ab7f3d0e5a85f88e4a0c7cc4d20d2ffed9
Austin Crim <crim.austin@principal.com>
2021-09-02 09:20:30 -0500

    redirect from login if valid session

--- src/hooks.js
+++ src/hooks.js
@@ -7,38 +7,48 @@ const AUTHORIZED_PATHS = ['/', '/budgets', '/transactions', '/api/transactions',
  * @type {import('@sveltejs/kit').Handle}
  */
 export const handle = async ({ request, resolve }) => {
-  const sessionCookie = request.headers.cookie
-    ? cookie.parse(request.headers.cookie)
-    : null
+	const sessionCookie = request.headers.cookie ? cookie.parse(request.headers.cookie) : null
 
-  if (AUTHORIZED_PATHS.includes(request.path)) {
-    if (!(await isValidSession(sessionCookie?.session_id))) {
-      return {
-        ...(await resolve(request)),
-        status: 302,
-        headers: {
-          location: '/login'
-        }
-      }
-    }
-    try {
-      await refreshSession(sessionCookie?.session_id)
-    } catch (e) {
-      console.error('swallowing session error')
-    }
-    const user = await getUser(sessionCookie?.session_id)
-    request.locals.user = {
-      email: user.email,
-      items: user.items.map((item) => ({
-        itemId: item.itemId
-      }))
-    }
-  }
+	const validSession = await isValidSession(sessionCookie?.session_id)
 
-  return resolve(request)
+	if (AUTHORIZED_PATHS.includes(request.path)) {
+		if (!validSession) {
+			return {
+				...(await resolve(request)),
+				status: 302,
+				headers: {
+					location: '/login'
+				}
+			}
+		}
+		try {
+			await refreshSession(sessionCookie?.session_id)
+		} catch (e) {
+			console.error('swallowing session error')
+		}
+		const user = await getUser(sessionCookie?.session_id)
+		request.locals.user = {
+			email: user.email,
+			items: user.items.map((item) => ({
+				itemId: item.itemId
+			}))
+		}
+	} else {
+		if (request.path === '/login' && isValidSession) {
+			return {
+				...(await resolve(request)),
+				status: 302,
+				headers: {
+					location: '/'
+				}
+			}
+		}
+	}
+
+	return resolve(request)
 }
 
 /** @type {import('@sveltejs/kit').GetSession} */
 export const getSession = (request) => {
-  return request.locals.user
+	return request.locals.user
 }
